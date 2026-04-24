5c27bd969555ca56b6864e8b5f3c7cca902b76fc
ToomChuan <caiting0920@gmail.com>
2026-04-24 17:29:27 +0800

    改正面错位

--- src/routes/body.tsx
+++ src/routes/body.tsx
@@ -41,8 +41,8 @@ interface Hotspot {
 }
 
 const frontHotspots: Hotspot[] = [
-  { part: "neck",     top: "24%", left: "50%" },
-  { part: "shoulder", top: "24%", left: "70%" },
+  { part: "neck",     top: "28%", left: "50%" },
+  { part: "shoulder", top: "32%", left: "60%" },
   { part: "abdomen",  top: "44%", left: "50%" },
   { part: "thigh",    top: "63%", left: "43%" },
   { part: "calf",     top: "80%", left: "55%" },
